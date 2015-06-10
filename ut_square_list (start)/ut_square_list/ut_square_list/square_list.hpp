584c61b457583af3f30351eeb9eb4a71a9a62479
bvisch <bvisch@gmail.com>
2015-06-09 23:36:40 -0400

    Insert is almost done. For real.

--- ut_square_list (start)/ut_square_list/ut_square_list/square_list.hpp
+++ ut_square_list (start)/ut_square_list/ut_square_list/square_list.hpp
@@ -25,15 +25,21 @@ public:
 private:
 	list<iterator> _head_list;
 	list<T> _body_list;
+	size_type side_length;
+	size_type size;
+	size_type head_size;
 
 	void resquare(head_iterator start);
+	iterator search(value_type element);
 
 public:
-	square_list();
-	~square_list();
-	iterator	insert(T element);
-	iterator	begin() { return _body_list.begin(); }
-	iterator	end() { return _body_list.end(); }
+	square_list() : side_length(0), size(0), head_size(0) {}
+	//~square_list();
+	iterator		binsert(iterator position, value_type element);
+	head_iterator	hinsert(head_iterator position, iterator element);
+	iterator		insert(value_type element);
+	iterator		begin() { return _body_list.begin(); }
+	iterator		end() { return _body_list.end(); }
 };
 
 //template <typename T>
@@ -50,19 +56,82 @@ public:
 
 template <typename T>
 void square_list<T>::resquare(head_iterator start) {
-	for (square_list::head_iterator it_head = start; it_head != _head_list.end(); ++it_head)
-		*it_head--;
+	if (head_size <= side_length) {
+		for (square_list::head_iterator it_head = start; it_head != _head_list.end(); ++it_head)
+			*it_head--;
+	}
+	else if (head_size > side_length) {
+		_head_list.clear();
+		unsigned count = 0;
+		for (square_list::iterator it_body = _body_list.begin(); it_body != _body_list.end(); ++it_body) {
+			if (count % side_length == 0) {
+				_head_list.push_back(it_body);
+			}
+			++count;
+		}
+	}
+}
+
+//template <typename T>
+//typename square_list<T>::iterator square_list<T>::search(value_type element) {
+//	for (square_list::head_iterator it_head = _head_list.begin(); it_head != _head_list.end(); ++it_head) {
+//		if (**it_head >= element) {
+//			for (square_list::reverse_iterator it_body = square_list::reverse_iterator(*it_head); it_body != _body_list.rend(); ++it_body) {
+//				if (*it_body < element) {
+//					return it_body--.base();
+//				}
+//			}
+//		}
+//	}
+//}
+
+template <typename T>
+typename square_list<T>::iterator square_list<T>::binsert(typename square_list<T>::iterator position, typename square_list<T>::value_type element) {
+	++size;
+	if (size > pow(head_size, 2)) {
+		++side_length;
+	}
+	return _body_list.insert(position, element);
 }
 
 template <typename T>
-typename square_list<T>::iterator square_list<T>::insert(T element) {
+typename square_list<T>::head_iterator square_list<T>::hinsert(typename square_list<T>::head_iterator position, typename square_list<T>::iterator element) {
+	++head_size;
+	return _head_list.insert(position, element);
+}
+
+template <typename T>
+typename square_list<T>::iterator square_list<T>::insert(typename square_list<T>::value_type element) {
+	if (_body_list.begin() == _body_list.end()) {
+		binsert(_body_list.begin(), element);
+		hinsert(_head_list.begin(), _body_list.begin());
+		return _body_list.begin();
+	}
+	//binsert(search(element), element);
+
+
 	for (square_list::head_iterator it_head = _head_list.begin(); it_head != _head_list.end(); ++it_head) {
 		if (**it_head > element) {
 			for (square_list::reverse_iterator it_body = square_list::reverse_iterator(*it_head); it_body != _body_list.rend(); ++it_body) {
+				auto asdf = prev(_body_list.rend());
 				if (*it_body < element) {
-					_body_list.insert(it_body--.base(), element);
+					binsert(--it_body.base(), element);
 					resquare(it_head);
-					return (it_body + 1).base();
+					return it_body.base();
+				}
+				else if (it_body == prev(_body_list.rend())) {
+					binsert(_body_list.begin(), element);
+					resquare(it_head);
+					return _body_list.begin();
+				}
+			}
+		}
+		else if (it_head == prev(_head_list.end())) {
+			for (square_list::iterator it_body = *it_head; it_body != _body_list.end(); ++it_body) {
+				if (*it_body > element) {
+					binsert(--it_body, element);
+					resquare(it_head);
+					return it_body;
 				}
 			}
 		}
