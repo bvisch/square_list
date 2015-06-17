77e20e40e18d6bd2b2e44cd0bc6dd734a618f39a
bvisch <bvisch@gmail.com>
2015-06-17 02:06:07 -0400

    Finished erase. Probably redundant if predicates aren't implemented.

--- ut_square_list (start)/ut_square_list/ut_square_list/square_list.hpp
+++ ut_square_list (start)/ut_square_list/ut_square_list/square_list.hpp
@@ -35,16 +35,34 @@ private:
 	list<iterator> _head_list;
 	list<T> _body_list;
 	size_type side_length;
-	search_result find(value_type element);
+	search_result _find(value_type element);
 	bool validateSort();
 
 public:
 	square_list() : side_length(0) {}
-	//~square_list();
-	size_type			size() { return _body_list.size(); }
-	iterator			insert(value_type element);
-	const_iterator		begin() { return _body_list.begin(); }
-	const_iterator		end() { return _body_list.end(); }
+	size_type					size() { return _body_list.size(); }
+	size_type					max_size() { return std::numeric_limits<size_type>::max(); }
+	bool						empty() { return _body_list.empty(); }
+
+	iterator					insert(value_type const& element);
+	iterator					insert(value_type&& element);
+	void						erase(iterator position);
+	void						clear();
+
+	iterator					begin() { return _body_list.begin(); }
+	iterator					end() { return _body_list.end(); }
+	const_iterator				begin() const { return _body_list.begin(); }
+	const_iterator				end() const { return _body_list.end(); }
+	const_iterator				cbegin() { return _body_list.cbegin(); }
+	const_iterator				cend() { return _body_list.cend(); }
+	reverse_iterator			rbegin() { return _body_list.rbegin(); }
+	reverse_iterator			rend() { return _body_list.rend(); }
+	const_reverse_iterator		rbegin() const { return _body_list.crbegin(); }
+	const_reverse_iterator		rend() const { return _body_list.crend(); }
+	const_reverse_iterator		crbegin() const { return _body_list.crbegin(); }
+	const_reverse_iterator		crend() const { return _body_list.crend(); }
+
+
 };
 
 template <typename T>
@@ -58,7 +76,7 @@ bool square_list<T>::validateSort() {
 }
 
 template <typename T>
-typename square_list<T>::search_result square_list<T>::find(typename square_list<T>::value_type element) {
+typename square_list<T>::search_result square_list<T>::_find(typename square_list<T>::value_type element) {
 	if (size() == 0) { return search_result(_head_list.begin(), _body_list.begin(), false); }
 	else if (_body_list.front() >= element) { return search_result(_head_list.begin(), _body_list.begin(), _body_list.front() == element); }
 	else if (_body_list.back() == element) { return search_result(_head_list.end(), --_body_list.end(), true); }
@@ -74,7 +92,7 @@ typename square_list<T>::search_result square_list<T>::find(typename square_list
 }
 
 template <typename T>
-typename square_list<T>::iterator square_list<T>::insert(typename square_list<T>::value_type element) {
+typename square_list<T>::iterator square_list<T>::insert(typename square_list<T>::value_type const& element) {
 	if (size() == 0) { 
 		_body_list.push_back(element);
 		_head_list.push_back(_body_list.begin());
@@ -82,7 +100,7 @@ typename square_list<T>::iterator square_list<T>::insert(typename square_list<T>
 		return _body_list.begin();
 	}
 
-	search_result res = find(element);
+	search_result res = _find(element);
 	_body_list.insert(res.pos, element);
 
 	while (res.head_pos != _head_list.end())
@@ -94,12 +112,86 @@ typename square_list<T>::iterator square_list<T>::insert(typename square_list<T>
 		for (unsigned i = 0; i < side_length - 1; ++i)
 			advance(*it_head++, i);
 	}
-
 	else if (size() % side_length == 1) {
 		_head_list.push_back(--_body_list.end());
 	}
 
 	return res.pos;
 }
+
+template <typename T>
+typename square_list<T>::iterator square_list<T>::insert(typename square_list<T>::value_type&& element) {
+	if (size() == 0) {
+		_body_list.push_back(element);
+		_head_list.push_back(_body_list.begin());
+		++side_length;
+		return _body_list.begin();
+	}
+
+	search_result res = _find(element);
+	_body_list.insert(res.pos, element);
+
+	while (res.head_pos != _head_list.end())
+		--(*res.head_pos++);
+
+	if (size() > pow(side_length, 2)) {
+		++side_length;
+		head_iterator it_head = _head_list.begin();
+		for (size_type i = 0; i < side_length - 1; ++i)
+			advance(*it_head++, i);
+	}
+	else if (size() % side_length == 1) {
+		_head_list.push_back(--_body_list.end());
+	}
+
+	return res.pos;
+}
+
+template <typename T>
+void square_list<T>::erase(typename square_list<T>::iterator position) {
+	if (&_body_list.front() == &_body_list.back()) { return clear(); }
+
+	head_iterator it_head = _head_list.begin();
+	while (it_head != _head_list.end() && **it_head < *position) { ++it_head; }		// find possible nearest head position
+	if (it_head != _head_list.begin()) { --it_head; }
+
+	// find the actual closest position within the list so reflow can occur
+	// worst case scenario: O(n)
+	// this might be redundant... an alternative would be to simply find the first occurrence of the element and remove it
+	iterator it_body;
+	bool pos_found = *it_head == position;
+	while (it_head != _head_list.end() && !pos_found) {
+		it_body = *it_head++;
+		while (it_body != _body_list.end() && it_body != position && it_head != _head_list.end() && it_body != *it_head && !pos_found) { 
+			pos_found = ++it_body == position; 
+		}
+	}
+
+	while (it_head != _head_list.end()) { ++(*it_head++); }
+
+	_body_list.erase(position);
+
+	if (size() <= pow(side_length - 1, 2)) {
+		--side_length;
+		head_iterator head_pos = _head_list.begin();
+		for (long i = 0; i < side_length; ++i)
+			advance(*head_pos++, -i); // signed-unsigned mismatch. fix this.
+	}
+	else if (size() % side_length == 0) {
+		_head_list.erase(--_head_list.end());
+	}
+
+	return;
+}
+
+template <typename T>
+void square_list<T>::clear() {
+	_head_list.clear();
+	_body_list.clear();
+	side_length = 0;
+	return;
+}
+
+
 
 #endif
